8ccb82b88f3d14e734c7f826edcacf770733b314
magg77 <escenariopaloma@gmail.com>
2025-02-01 21:14:28 -0500

    Applying scroll behavior to the top app bar of Android Studio and adding menu with search icon

--- app/src/main/java/com/maggiver/movieshd/core/valueObject/AdapterMovies.kt
+++ app/src/main/java/com/maggiver/movieshd/core/valueObject/AdapterMovies.kt
@@ -2,9 +2,12 @@ package com.maggiver.movieshd.core.valueObject
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.maggiver.movieshd.R
 import com.maggiver.movieshd.core.utils.BaseViewHolder
 import com.maggiver.movieshd.core.utils.Constants
 import com.maggiver.movieshd.databinding.MovieCardViewBinding
@@ -12,22 +15,47 @@ import com.maggiver.movieshd.homeMovie.data.provider.remote.model.MovieCustom
 
 class AdapterMovies(
     private val context: Context,
-    private val moviesList: List<MovieCustom>,
+    private val moviesList: List<Any>,
     private val onItemClickListener: (MovieCustom) -> Unit
 ) : RecyclerView.Adapter<BaseViewHolder<*>>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
-        val itemBinding = MovieCardViewBinding.inflate(LayoutInflater.from(context), parent, false)
-        return MainViewHolder(itemBinding)
+        val inflater = LayoutInflater.from(context)
+        return when (viewType) {
+
+            VIEW_TYPE_HEADER -> {
+                val view = inflater.inflate(R.layout.item_header, parent, false)
+                HeaderViewHolder(view)
+            }
+
+            VIEW_TYPE_MOVIE -> {
+                val itemBinding = MovieCardViewBinding.inflate(inflater, parent, false)
+                MainViewHolder(itemBinding)
+            }
+
+            else -> throw IllegalArgumentException("Invalid view type")
+        }
     }
 
-    override fun getItemCount(): Int {
-        return moviesList.size
+    override fun getItemCount(): Int = moviesList.size
+
+
+    override fun getItemViewType(position: Int): Int {
+        return if (moviesList[position] is String) VIEW_TYPE_HEADER else VIEW_TYPE_MOVIE
     }
 
     override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
         when (holder) {
-            is MainViewHolder -> holder.bind(moviesList[position], position)
+            is HeaderViewHolder -> holder.bind(moviesList[position] as String, position)
+            is MainViewHolder -> holder.bind(moviesList[position] as MovieCustom, position)
+        }
+    }
+
+    // ViewHolder para el Header
+    inner class HeaderViewHolder(view: View) : BaseViewHolder<String>(view) {
+        private val title: TextView = view.findViewById(R.id.headerTitle)
+        override fun bind(item: String, position: Int) {
+            title.text = item
         }
     }
 
@@ -45,12 +73,16 @@ class AdapterMovies(
                 item.overview.takeIf { it.isNotBlank() } ?: "Sin descripción disponible."
 
             binding.contentCardView.setOnClickListener {
-                onItemClickListener(moviesList[position])
+                onItemClickListener(item)
             }
 
         }
         
     }
 
+    companion object {
+        const val VIEW_TYPE_HEADER = 0
+        private const val VIEW_TYPE_MOVIE = 1
+    }
 
 }
--- app/src/main/java/com/maggiver/movieshd/homeMovie/ui/HomeFragment.kt
+++ app/src/main/java/com/maggiver/movieshd/homeMovie/ui/HomeFragment.kt
@@ -3,9 +3,16 @@ package com.maggiver.movieshd.homeMovie.ui
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.MenuRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -14,6 +21,7 @@ import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.maggiver.movieshd.R
 import com.maggiver.movieshd.core.valueObject.AdapterMovies
 import com.maggiver.movieshd.core.valueObject.ResourceState
 import com.maggiver.movieshd.databinding.FragmentHomeBinding
@@ -26,6 +34,7 @@ import kotlinx.coroutines.launch
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
+
     // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
     private val viewModelPopularMovie by viewModels<NowPlayingViewModel>()
@@ -43,7 +52,8 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        setupLayots()
+        setupLayoutsMaterialToolbar()
+        setupLayoutsAdapter()
         setupObservers()
 
     }
@@ -53,22 +63,61 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 
+
     //custom methods *******************************************************************************
 
-    private fun setupLayots() {
-        binding.rvHomeFragment.layoutManager = GridLayoutManager(
-            requireContext(), 2, LinearLayoutManager.VERTICAL, false
-        )
+    private fun setupLayoutsMaterialToolbar() {
+
+        // this is compulsory in order to get behavior of expand/collapse
+        (requireActivity() as AppCompatActivity).setSupportActionBar(binding.materialToolbar)
+
+        setupMenu(R.menu.top_app_bar) { menuItem ->
+            when (menuItem.itemId) {
+                R.id.search -> {
+                    //Logica
+                    Log.i("searchMoviesHD", "buscar movie click")
+
+                    true
+                }
+
+                else -> false
+
+            }
+        }
+
+    }
+
+    private fun Fragment.setupMenu(@MenuRes menuId: Int, onMenuSelected: (MenuItem) -> Boolean) =
+        (requireActivity() as MenuHost).addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) =
+                menuInflater.inflate(menuId, menu)
+
+            override fun onMenuItemSelected(menuItem: MenuItem) = onMenuSelected(menuItem)
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
+    private fun setupLayoutsAdapter() {
+
+        val layoutManager = GridLayoutManager(requireContext(), 2)
+        layoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int {
+                val adapter = binding.rvHomeFragment.adapter as? AdapterMovies
+                return if (adapter?.getItemViewType(position) == AdapterMovies.VIEW_TYPE_HEADER) 2 else 1
+            }
+        }
+        binding.rvHomeFragment.setHasFixedSize(true)
+        binding.rvHomeFragment.isNestedScrollingEnabled = false // Deshabilita el scrolling del RecyclerView dentro del NestedScrollView
+        binding.rvHomeFragment.overScrollMode = View.OVER_SCROLL_NEVER
+        binding.rvHomeFragment.layoutManager = layoutManager
     }
 
     private fun setupObservers() {
 
         viewModelPopularMovie.nowPlayingMovies(requireContext())
         viewLifecycleOwner.lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED){
-                viewModelPopularMovie.uiState.collect(){
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModelPopularMovie.uiState.collect() {
 
-                    when(it){
+                    when (it) {
 
                         is ResourceState.LoadingState -> {
                             binding.psHome.visibility = View.VISIBLE
@@ -80,13 +129,14 @@ class HomeFragment : Fragment() {
                             binding.rvHomeFragment.adapter =
                                 AdapterMovies(
                                     context = requireContext(),
-                                    moviesList = it.data,
+                                    moviesList = listOf("Ver ahora") + it.data,
                                     onItemClickListener = { dataResult ->
-                                            Log.i("movieclick", "$dataResult")
+                                        Log.i("movieclick", "$dataResult")
 
-                                        val action = HomeFragmentDirections.actionNavigationHomeToDetailMovieFragmentFullScreen(
-                                            movieCustom = dataResult
-                                        )
+                                        val action =
+                                            HomeFragmentDirections.actionNavigationHomeToDetailMovieFragmentFullScreen(
+                                                movieCustom = dataResult
+                                            )
                                         findNavController().navigate(action)
                                     }
                                 )
@@ -111,8 +161,6 @@ class HomeFragment : Fragment() {
             }
         }
     }
-
-
 
 
 }
